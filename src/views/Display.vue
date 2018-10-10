9a48f32ed1b1dfc32650ffdc5d9fb79e66e6bd1c
cnelson-ma <cnelson@mccurdyauction.com>
2018-10-10 14:44:37 -0500

    Prep for push to remote

--- src/views/Display.vue
+++ src/views/Display.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="display">
-    <LeaderBoard/>
+    <p>Display</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import LeaderBoard from '@/components/LeaderBoard.vue';
 
 export default {
   name: 'display',
   components: {
-    LeaderBoard,
   },
 };
 </script>
